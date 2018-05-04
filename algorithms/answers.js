1d0c067a46e124dcf8d8e8b94a5ef20a9e867aca
Cliff Kang <inlovewithhim@gmail.com>
2018-05-04 10:11:37 -0700

    finished Exercise I for algs

---
+++ algorithms/answers.js
@@ -0,0 +1,19 @@
+// Exercise I
+
+const a = O(n^3);
+const b = O(log(n)); // may go forever, dependent on what 'x' is
+const c = ((((n ^ (1/2)) / 2) * 8) * 8) = 32(n ^ (1/2)) = O(n^0.5) = O(n);
+const d = O(n) || O(nlog(n)) // the first for loop feels like a log(n) so maybe second one
+const e = n + 0.5(n) + 0.25(n) + 10 | 0.75(n) + 10 | O(n);
+const f;
+{
+   if (n >= 0) {
+      f = O(Infinity);
+   }
+   else f = O(n);
+}
+const g = O(n - target) = O(n);
+
+// Exercise II
+
+
